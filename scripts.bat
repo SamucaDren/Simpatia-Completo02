307a9129606e87f1c597ace6b4b698bebeda0744
Samuel Nogueira <samucans2004@gmail.com>
2026-03-05 13:15:21 -0300

    teste de commit4

--- scripts.bat
+++ scripts.bat
@@ -5,7 +5,11 @@ cd ajuda_ai
 :: Faz o build
 npm run build
 
-:: Copia a build para a pasta de deploy relativa ao projeto
+:: Apaga tudo na pasta de destino e recria
+rd "%~dp0\simpatia\ajuda-ai" /s /q
+mkdir "%~dp0\simpatia\ajuda-ai"
+
+:: Copia a build
 xcopy build "%~dp0\simpatia\ajuda-ai" /E /H /Y
 
 :: Sem pause, para não travar o commit
