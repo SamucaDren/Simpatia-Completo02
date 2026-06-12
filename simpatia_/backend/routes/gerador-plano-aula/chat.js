0415921b6282b288c40663ae5634db4fad42daa3
SamucaDren <samucans2004@gmail.com>
2026-06-12 02:47:52 -0300

    Teste #02 - Gerador de Planos de Aula

--- simpatia_/backend/routes/gerador-plano-aula/chat.js
+++ simpatia_/backend/routes/gerador-plano-aula/chat.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 
+const OpenAI = require("openai");
+
 const groq = new OpenAI({
   apiKey: process.env.GROQ_API_KEY,
   baseURL: "https://api.groq.com/openai/v1",
