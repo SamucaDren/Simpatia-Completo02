82bbce61415374ea9774a6fd58bc9c7cc2fd090a
SamucaDren <samucans2004@gmail.com>
2026-06-13 16:54:39 -0300

    Tentativa #02 - Quiz Simpatia

--- quiz-simpatia/vite.config.js
+++ quiz-simpatia/vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/quiz-simpatia/",
+
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         changeOrigin: true,
       },
-      '/uploads': {
-        target: 'http://localhost:3001',
+      "/uploads": {
+        target: "http://localhost:3001",
         changeOrigin: true,
       },
     },
