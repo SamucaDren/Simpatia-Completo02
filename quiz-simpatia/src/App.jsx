fad11edfdca6559fa1692e99d0e599947145aed1
SamucaDren <samucans2004@gmail.com>
2026-06-13 20:07:25 -0300

    Tentativa #03 - Quiz Simpatia

--- quiz-simpatia/src/App.jsx
+++ quiz-simpatia/src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Home      from './pages/Home';
-import Quiz      from './pages/Quiz';
-import Resultado from './pages/Resultado';
-import Dashboard from './pages/Dashboard';
-import Revisao   from './pages/Revisao';
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Resultado from "./pages/Resultado";
+import Dashboard from "./pages/Dashboard";
+import Revisao from "./pages/Revisao";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/quiz-simpatia">
       <Routes>
-        <Route path="/"            element={<Home />} />
-        <Route path="/quiz"        element={<Quiz />} />
-        <Route path="/resultado"   element={<Resultado />} />
-        <Route path="/dashboard"   element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/resultado" element={<Resultado />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/revisao/:id" element={<Revisao />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
